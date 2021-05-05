560fc1c1f444641e959409be4e2bb4450a6faea6
milenashirinyan <milenashirinyan8030@gmail.com>
2021-05-05 23:46:50 +0400

    Chess Board With JavaScript
    Chess Board With JavaScript

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//создать поле и ячейки
+let field = document.createElement('div');
+document.body.appendChild(field);
+field.classList.add('field'); 
+
+for (let i=1; i<65; i++){
+    let excel = document.createElement('div');
+    field.appendChild(excel);
+    excel.classList.add('excel');
+}
+
+let excel = document.getElementsByClassName('excel');
+//console.log(excel);
+let x=1, y=8
+for (let i=0; i<excel.length; i++){
+    if (x>8){
+        x=1;
+        y--
+    }
+    excel[i].setAttribute('posX', x);
+    excel[i].setAttribute('posY', y);
+    x++;
+    if ((i % 2 == 0 && y % 2 == 0) || (i % 2 != 0 && y % 2 != 0)){
+        excel[i].style.backgroundColor = "rgb(247, 235, 213)"
+    } else {
+        excel[i].style.backgroundColor = "rgb(37, 22, 3)"
+    }
+}
